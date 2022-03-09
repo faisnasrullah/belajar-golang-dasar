d56f3462364b81e05246df3d4f9bde38b8b46825
Fais Nasrullah <fais@automatalabs.co>
2022-03-09 14:50:15 +0700

    cara lain menulis variabel pada go

--- 5_variabel.go
+++ 5_variabel.go
@@ -8,11 +8,24 @@ func main() {
 		- Hanya bisa menyimpan tipe data yang sama
 		- Untuk membuat variabel, kita harus menggunakan kata kunci var, lalu diikuti nama variabel dan tipe datanya
 	*/
+	// cara pertama
 	var name string
+
+	// cara kedua (golang akan secara otomatis tau tipe datanya apa)
+	var email = "fais@automatalabs.co"
+	var age byte = 23
+
+	// cara ketiga (tanpa menggunakan kata kunci var)
+	country := "Indonesia"
 
 	name = "Fais Nasrullah"
 	fmt.Println(name)
+	fmt.Println(email)
+	fmt.Println(age)
+	fmt.Println(country)
 
 	name = "Eko Sungkowo"
+	country = "Singapore"
 	fmt.Println(name)
+	fmt.Println(country)
 }
