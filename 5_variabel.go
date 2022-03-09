ba50c734e56c2800059f34216a372b5779ef6295
Fais Nasrullah <fais@automatalabs.co>
2022-03-09 14:40:11 +0700

    Variabel pada Go

---
+++ 5_variabel.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Variabel pada Go :
+		- Hanya bisa menyimpan tipe data yang sama
+		- Untuk membuat variabel, kita harus menggunakan kata kunci var, lalu diikuti nama variabel dan tipe datanya
+	*/
+	var name string
+
+	name = "Fais Nasrullah"
+	fmt.Println(name)
+
+	name = "Eko Sungkowo"
+	fmt.Println(name)
+}
