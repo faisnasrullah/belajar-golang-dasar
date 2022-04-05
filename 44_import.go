b33b8e1b125f4fdf573fd86bafb8b5fb91a5448f
Fais Nasrullah <fais@automatalabs.co>
2022-04-05 09:50:22 +0700

    Access Modifier pada Go

--- 44_import.go
+++ 44_import.go
@@ -8,9 +8,20 @@ import (
 /**
 Package :
 - mengakses package helper dari file lain kita harus menggunakan import
+
+Access Modifier :
+- kita sudah buat beberapa case di package helper
+- variabel yg bisa diakases -> Application
+- variabel yg tidak bisa diakases -> version
+- function yg bisa diakases -> SayHello
+- function yg tidak bisa diakases -> sayGoodbye
 */
 
 func main() {
 	result := helper.SayHello("Jhon Wick")
 	fmt.Println(result)
+	fmt.Println(helper.Application)
+	// ACCESS MODIFIER (yang tidak bisa diakses)
+	// goodbye := helper.sayGoodBye("Jhon") // error karena tidak bisa diakses
+	// fmt.Println(helper.version) // error karena tidak bisa diakses
 }
--- helper/sayhello.go
+++ helper/sayhello.go
@@ -7,6 +7,20 @@ Package :
 - dalam satu folder, tidak bisa menggunakan nama package yg sama
 */
 
+// ACCESS MODIFIER :
+// penggunaan nama variabel, jika diawali dengan HURUF BESAR, maka bisa diakses di package / file lain
+// begitupun sebaliknya, jika diawali dengan HURUF KECIL, maka tidak bisa diakses di package / file lain
+var version = 1
+var Application = "Belajar Golang Dasar"
+
+// ACCESS MODIFIER :
+// karena nama function diawali dengan HURUF BESAR, maka bisa diakses di package / file lain
 func SayHello(name string) string {
 	return "Hellow " + name
+}
+
+// ACCESS MODIFIER :
+// karena nama function diawali dengan HURUF KECIL, maka tidak bisa diakses di package / file lain
+func sayGoodBye(name string) string {
+	return "Goodbye " + name
 }
